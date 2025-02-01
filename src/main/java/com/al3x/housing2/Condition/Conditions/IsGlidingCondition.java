72b02eb1b640b9bab5835c87664a5346e306bd2e
Wekend <96594839+Wekendd@users.noreply.github.com>
2025-02-01 15:09:37 -0800

    Add IsGlidingCondition

---
+++ src/main/java/com/al3x/housing2/Condition/Conditions/IsGlidingCondition.java
@@ -0,0 +1,53 @@
+package com.al3x.housing2.Condition.Conditions;
+
+import com.al3x.housing2.Condition.Condition;
+import com.al3x.housing2.Instances.HousingWorld;
+import com.al3x.housing2.Utils.ItemBuilder;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+
+public class IsGlidingCondition extends Condition {
+
+        public IsGlidingCondition() {
+            super("Is Gliding");
+        }
+
+        @Override
+        public String toString() {
+            return "IsGlidingCondition";
+        }
+
+        @Override
+        public void createDisplayItem(ItemBuilder builder) {
+            builder.material(Material.FEATHER);
+            builder.name("&eIs Gliding");
+            builder.description("Check if the player is gliding with an elytra.");
+            builder.rClick(ItemBuilder.ActionType.REMOVE_YELLOW);
+            builder.shiftClick();
+        }
+
+        @Override
+        public void createAddDisplayItem(ItemBuilder builder) {
+            builder.material(Material.FEATHER);
+            builder.name("&eIs Gliding");
+            builder.description("Check if the player is gliding with an elytra.");
+            builder.lClick(ItemBuilder.ActionType.ADD_YELLOW);
+        }
+
+        @Override
+        public boolean execute(Player player, HousingWorld house) {
+            return player.isGliding();
+        }
+
+        @Override
+        public HashMap<String, Object> data() {
+            return new HashMap<>();
+        }
+
+        @Override
+        public boolean requiresPlayer() {
+            return true;
+        }
+}
